25100c917c5222b31b7d29334d114ad24e313323
d-mitrofanov-v <dmitriy.mitrofanov.v@gmail.com>
2025-07-20 17:23:30 +0300

    add template for index page

--- public/index.php
+++ public/index.php
@@ -3,14 +3,21 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI\Container;
+use Slim\Views\PhpRenderer;
 
 require __DIR__ . "/../vendor/autoload.php";
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new PhpRenderer(__DIR__ . "/../templates");
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    return $response->write('Hello world');
+    return $this->get('renderer')->render($response, 'home.phtml');
 });
 
 $app->run();
